836fbe515a71d67a21c55459dd2124f905413014
Akiva Miura <akiva.miura@gmail.com>
2017-07-12 08:05:47 +0900

    Ported almost class methods of "cedarpp.h"

--- pycedar/pycedar.pxd
+++ pycedar/pycedar.pxd
@@ -1,10 +1,14 @@
-# distutils: language=c++
-# -*- coding: utf-8 -*-
+from libcpp cimport bool
 
 cdef extern from "cedarpp.h" namespace "cedar":
-    ctypedef npos_t
-    #ctypedef result_triple_type
-    cdef cppclass da[ValueType]:
+    #ctypedef npos_t
+    ctypedef unsigned long npos_t
+    cdef cppclass da[value_type]:
+        struct result_triple_type:
+            value_type value
+            size_t     length
+            npos_t     id
+
         da()
         size_t capacity   () const
         size_t size       () const
@@ -15,9 +19,46 @@ cdef extern from "cedarpp.h" namespace "cedar":
         size_t nonzero_length () const
         size_t num_keys () const
 
-        ResultType exactMatchSearch[ResultType] (const char* key, size_t len, npos_t from_) const
+        result_type exactMatchSearch[result_type] (const char* key, size_t len, npos_t from_) const
 
-        size_t commonPrefixSearch[ResultType] (const char* key, ResultType* result, size_t result_len, size_t len, npos_t from_) const
+        size_t commonPrefixSearch[result_type] (const char* key, result_type* result, size_t result_len, size_t len, npos_t from_) const
+
+        #template <typename T>
+        #size_t commonPrefixPredict (const char* key, T* result, size_t result_len)
+
+        void suffix (char* key, size_t len, npos_t to) const
+            
+        value_type traverse (const char* key, npos_t& from_, size_t& pos) const
+
+        #struct empty_callback { void operator () (const int, const int) {} }; // dummy empty function
+
+        value_type& update(const char* key)
+        value_type& update(const char* key, size_t len, value_type val)
+        value_type& update(const char* key, npos_t& from_, size_t& pos, size_t len, value_type val)
+
+        #int erase (const char* key) { return erase (key, std::strlen (key)); }
+        #int erase (const char* key, size_t len, npos_t from = 0)
+
+        #int build (size_t num, const char** key, const size_t* len = 0, const value_type* val = 0) {
+
+        #template <typename T>
+        #void dump (T* result, const size_t result_len) {
+
+        #void shrink_tail () {
+
+        int save (const char* fn, const char* mode, const bool shrink)
+
+        int open (const char* fn, const char* mode, const size_t offset, size_t size_)
+
+        #void restore () { // restore information to update
+
+        #void set_array (void* p, size_t size_ = 0) { // ad-hoc
+
+        #const void* array () const { return _array; }
+
+        int begin (npos_t& from_, size_t& len)
+
+        int next (npos_t& from_, size_t& len, const npos_t root)
 
         #ctypedef result_triple_type
         #cdef cppclass result_triple_type
--- pycedar/pycedar.pyx
+++ pycedar/pycedar.pyx
@@ -1,13 +1,56 @@
 # distutils: language=c++
 # -*- coding: utf-8 -*-
 
-from pycedar cimport da
-#from pycedar cimport result_triple_type
+import sys
 
 from cython.operator cimport dereference as deref
 #from cython.operator cimport reference as ref
-from libcpp.vector cimport vector
 from libcpp.string cimport string
+from libcpp.vector cimport vector
+
+from pycedar cimport da
+from pycedar cimport npos_t
+#from pycedar cimport result_triple_type
+
+#cdef bytes py3_str2bytes(str s):
+#    return bytes(s, 'utf-8')
+#cdef bytes py2_str2bytes(bytes s):
+#    # bytes == str
+#    return s
+#
+#cdef str py3_bytes2str(bytes s):
+#    return s.decode('utf-8')
+#cdef str py2_str2bytes(str s):
+#    # bytes == str
+#    return s
+#
+#cdef str2bytes
+#cdef bytes2str
+#if sys.version_info.major >= 3:
+#    str2bytes = py3_str2bytes
+#    bytes2str = py3_bytes2str
+#else:
+#    str2bytes = py2_str2bytes
+#    bytes2str = py2_bytes2str
+#
+cdef bytes to_bytes(s):
+    if isinstance(s, bytes):
+        return s
+    elif isinstance(s, str):
+        return bytes(s, 'utf-8')
+    else:
+        raise TypeError("expected str of bytes, but given: %s" % type(s).__name__)
+
+cdef str to_str(s):
+    if isinstance(s, str):
+        return s
+    elif isinstance(s, bytes):
+        return s.decode('utf-8')
+    else:
+        raise TypeError("expected str of bytes, but given: %s" % type(s).__name__)
+
+#cdef result_to_tuple(da[int].result_triple_type result):
+#    return result.value, result.length, result.id
 
 cdef class trie:
     cdef da[int] obj
@@ -19,37 +62,65 @@ cdef class trie:
     def __len__(self):
         return self.obj.num_keys()
 
-    def capacity(self):
+    cpdef capacity(self):
         return self.obj.capacity()
-    def size(self):
+    cpdef size(self):
         return self.obj.size()
-    def length(self):
+    cpdef length(self):
         return self.obj.length()
-    def total_size(self):
+    cpdef total_size(self):
         return self.obj.total_size()
-    def unit_size(self):
+    cpdef unit_size(self):
         return self.obj.unit_size()
-    def nonzero_size(self):
+    cpdef nonzero_size(self):
         return self.obj.nonzero_size()
-    def nonzero_length(self):
+    cpdef nonzero_length(self):
         return self.obj.nonzero_length()
-    def num_keys(self):
+    cpdef num_keys(self):
         return self.obj.num_keys()
 
-    #def exactMatchSearch(self, key, start=0):
-    #    return self.obj.exactMatchSearch[int] (key, len(key), start)
+    cpdef lookup(self, key, start=0):
+        cdef bytes k = to_bytes(key)
+        return self.obj.exactMatchSearch[int] (k, len(k), start)
 
-    def commonPrefixSearch(self, key, start=0):
-        pass
-        #cdef vector[da[int].result_triple_type] result
-        #cdef vector[char] buf
-        #cdef string buf
-        #cdef string* ptr
-        #result.reserve(len(key))
-        #ptr = &buf
-        #buf.resize(len(key))
-        #size_t commonPrefixSearch[ResultType] (const char* key, ResultType* result, size_t result_len, size_t len, npos_t from_) const
-        #num = self.obj.commonPrefixSearch[string] (key, &buf, len(key), len(key), start)
-        #num = self.obj.commonPrefixSearch[string] (key, &result, len(key), len(key), start)
+    cpdef prefix(self, key, size=1, start=0):
+        cdef bytes k = to_bytes(key)
+        cdef vector[da[int].result_triple_type] result_vector
+        cdef list result_list = []
+        cdef da[int].result_triple_type r
+        cdef size_t ret
+        result_vector.resize(len(k))
+        ret = self.obj.commonPrefixSearch[da[int].result_triple_type] (k, &result_vector[0], len(k), len(k), start)
+        for r in result_vector:
+            if len(result_list) < ret:
+                result_list.append( (self.suffix(r.id,r.length), r.value, r.id) )
+        return result_list
 
+    cpdef str suffix(self, node_id, length):
+        cdef bytes buf = b'\0' * length
+        self.obj.suffix(buf, length, node_id)
+        return to_str(buf)
+
+    def traverse(self, key, npos_t start=0, size_t pos=0):
+        cdef bytes k = to_bytes(key)
+        cdef int result = self.obj.traverse(k, start, pos)
+        return result, start, pos
+
+    def update(self, key, value=0):
+        cdef bytes k = to_bytes(key)
+        return self.obj.update(k, len(k), value)
+
+    cpdef save(self, filepath, mode = 'wb', bool shrink = False):
+        return self.obj.save(to_bytes(filepath), to_bytes(mode), shrink) 
+
+    cpdef open(self, filepath, mode = 'rb', size_t offset = 0, size_t size = 0):
+        return self.obj.open(to_bytes(filepath), to_bytes(mode), offset, size)
+
+    def begin(self, npos_t start=0, size_t length=0):
+        cdef int result = self.obj.begin(start, length)
+        return result, start, length
+
+    def next(self, npos_t start, size_t length, npos_t root=0):
+        cdef result = self.obj.next(start, length, root)
+        return result, start, length
 
