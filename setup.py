92cf02714d77c866c745cbd44ffd5c4dfa59f205
Akiva Miura <akiva@eden.local>
2018-08-06 06:41:13 +0900

    Modified for PyPI registration

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
-from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from setuptools import find_packages
+from setuptools import setup
+from setuptools import Extension
 
 ext_modules=[
     Extension(
-        "pycedar",
-        ["pycedar/pycedar.pyx"],
-        include_dirs = ["./cedar/src"],
-        language="c++",
+        'pycedar',
+        ['pycedar/pycedar.pyx'],
+        include_dirs = ['./cedar/src'],
+        language='c++',
     )
 ]
 
 setup(
-    name = "pycedar",
-    version = "0.0.1",
-    cmdclass = {"build_ext": build_ext},
-    ext_modules = ext_modules
+    name = 'pycedar',
+    version = '0.0.4',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = ext_modules,
+    packages = find_packages(),
+    description = 'Python binding of cedar (implementation of efficiently-updatable double-array trie) using Cython',
+    url = 'https://github.com/akivajp/pycedar',
+    author = 'Akiva Miura',
+    author_email = 'akiva.miura@gmail.com',
+    license = 'GPLv2, GPLv2.1 and BSD',
 )
 
